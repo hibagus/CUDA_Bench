a09e9f895dc26aa6998b2bc0282c0b54b9ec1772
Bagus Hanindhito <git@bagus.my.id>
2022-10-03 17:05:16 -0500

    add argparse

--- cmake/ExternalLibs.cmake
+++ cmake/ExternalLibs.cmake
@@ -1,9 +1,17 @@
 include (ExternalProject)
-# NVBench
-ExternalProject_Add(NVBench
+# nvBench
+ExternalProject_Add(nvbench
     PREFIX ${CMAKE_SOURCE_DIR}/3rdParty/nvbench/build
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rdParty/nvbench/
     CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/libs/nvbench
     -DCMAKE_CUDA_ARCHITECTURES=${GPU_ARCHITECTURE_SUPPORT}
+)
+
+# argparse
+ExternalProject_Add(argparse
+    PREFIX ${CMAKE_SOURCE_DIR}/3rdParty/argparse/build
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rdParty/argparse/
+    CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/libs/argparse
 )
