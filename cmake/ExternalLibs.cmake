a465f0e6798a10ba80ef5971254de8e3b1bdd798
Bagus Hanindhito <git@bagus.my.id>
2022-10-04 16:05:39 -0500

    modify how external libs are built

--- cmake/ExternalLibs.cmake
+++ cmake/ExternalLibs.cmake
@@ -3,7 +3,7 @@ include (ExternalProject)
 # nvBench
 add_library(external_nvbench INTERFACE)
 ExternalProject_Add(nvbench
-    PREFIX ${CMAKE_SOURCE_DIR}/3rdParty/nvbench/build
+    PREFIX ${CMAKE_SOURCE_DIR}/build/nvbench/build
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rdParty/nvbench/
     CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/libs/nvbench
@@ -17,7 +17,7 @@ target_include_directories(external_nvbench
 # argparse
 add_library(external_argparse INTERFACE)
 ExternalProject_Add(argparse
-    PREFIX ${CMAKE_SOURCE_DIR}/3rdParty/argparse/build
+    PREFIX ${CMAKE_SOURCE_DIR}/build/argparse/build
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rdParty/argparse/
     CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/libs/argparse
@@ -28,16 +28,20 @@ target_include_directories(external_argparse
 # cutlass
 add_library(external_cutlass INTERFACE)
 ExternalProject_Add(cutlass
-    PREFIX ${CMAKE_SOURCE_DIR}/3rdParty/cutlass/build
+    PREFIX ${CMAKE_SOURCE_DIR}/build/cutlass/build
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rdParty/cutlass/
     CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/libs/cutlass
     -DCUTLASS_NVCC_ARCHS=${GPU_ARCHITECTURE_SUPPORT}
     -DCUTLASS_ENABLE_TOOLS_INIT=OFF
+    -DCUTLASS_ENABLE_TOOLS=OFF
     -DCUTLASS_ENABLE_LIBRARY_INIT=OFF
+    -DCUTLASS_ENABLE_LIBRARY=OFF
     -DCUTLASS_ENABLE_EXAMPLES_INIT=OFF
+    -DCUTLASS_ENABLE_EXAMPLES=OFF
     -DCUTLASS_ENABLE_TESTS_INIT=OFF
     -DCUTLASS_ENABLE_TESTS=OFF
+    -DCUTLASS_ENABLE_PROFILER=OFF
 
 )
 target_include_directories(external_cutlass
